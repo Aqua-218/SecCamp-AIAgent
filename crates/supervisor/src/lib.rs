a81ac3ac25525228ac695699f9941f7f16f4cb36
Aqua-218 <keiya_26n1102992@nnn.ed.jp>
2026-08-13 06:48:40 +0000

    fix(supervisor): hide obsolete socket path environment

--- crates/supervisor/src/lib.rs
+++ crates/supervisor/src/lib.rs
@@ -21,9 +21,8 @@ mod supervisor;
 
 #[cfg(target_os = "linux")]
 pub use linux_host::{
-    LinuxHostConfig, LinuxHostError, LinuxHostResources, WORKLOAD_CONTROL_SOCKET_ENV,
-    WORKLOAD_MOUNTPOINT_ENV, WORKLOAD_SUBJECT_ENV, WorkloadIsolationConfig,
-    WorkloadIsolationLimits,
+    LinuxHostConfig, LinuxHostError, LinuxHostResources, WORKLOAD_MOUNTPOINT_ENV,
+    WORKLOAD_SUBJECT_ENV, WorkloadIsolationConfig, WorkloadIsolationLimits,
 };
 
 #[cfg(target_os = "linux")]
